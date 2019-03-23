5b78f9f905a1033aaa07afcf37756ab80eaf4a28
pnminh <pnminh232@gmail.com>
2019-03-22 23:20:59 -0500

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

2af34df9cd2f9c8108919f7d55879f85b48c8ead
pnminh <pnminh232@gmail.com>
2019-03-23 19:18:27 -0500

    manual route

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
   constructor() {}
+  @Output("selectEmitter") selectEmitter = new EventEmitter();
+  selectPage(option: string) {
+    this.selectEmitter.emit(option);
+  }
 }

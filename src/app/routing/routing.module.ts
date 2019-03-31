09661a8659ad3ce8e95b35a397c9cd844e5e42bf
pnminh <pnminh232@gmail.com>
2019-03-30 23:50:10 -0500

    preload lazy loaded modules

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes, PreloadAllModules } from "@angular/router";
 
-import { AuthGuardService } from '../auth/auth-guard.service';
-import { HomeComponent } from '../home/home.component';
-import { ShoppingListComponent } from './../shopping-list/shopping-list.component';
+import { AuthGuardService } from "../auth/auth-guard.service";
+import { HomeComponent } from "../home/home.component";
+import { ShoppingListComponent } from "./../shopping-list/shopping-list.component";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
-    pathMatch:"full"
+    pathMatch: "full"
   },
   {
     path: "recipes",
-    loadChildren: '../recipes/recipes.module#RecipesModule',
+    loadChildren: "../recipes/recipes.module#RecipesModule"
   },
   {
     path: "shopping-list",
@@ -27,7 +27,9 @@ const routes: Routes = [
 ];
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class RoutingModule {}
